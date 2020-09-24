00ca73566c6777bf332d0d13f463725436f0beb0
李锦荣 <1813577274@qq.com>
2020-09-24 21:57:42 +0800

    查询字符串

--- bookmanager02/book/urls.py
+++ bookmanager02/book/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from book import views
 urlpatterns = [
     path('<cat_id>/<goods_id>', views.index),
+    path('string/', views.string),
 ]
--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 from book.models import BookInfo
 
+
 # url 路径参数
 def index(request, cat_id, goods_id):
     return JsonResponse({'cat_id': cat_id, 'id': goods_id})
 
+
 # 查询字符串
 def string(request):
-    
-
-    return
+    print(request.GET)
+    return HttpResponse('ok')
