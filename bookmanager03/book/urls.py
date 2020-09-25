affe56eecaa06b49ec7e6d9ecac0a289501125d0
李锦荣 <1813577274@qq.com>
2020-09-25 20:33:29 +0800

    项目练习

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,6 +1,27 @@
-from django.urls import path
+from django.urls import path, register_converter
 from book import views
+
+
+class MobileConverter:
+    """自定义路由转换器：匹配手机号"""
+    # 匹配手机号码的正则
+    regex = '1[3-9]\d{9}'
+
+    def to_python(self, value):
+        # 将匹配结果传递到视图内部时使用
+        return int(value)
+
+    def to_url(self, value):
+        # 将匹配结果用于反向解析传值时使用
+        return str(value)
+
+
+register_converter(MobileConverter, 'model')
 
 urlpatterns = [
     path('index/', views.index),
+    path('<int:cat_id>/<model:goods_id>', views.goods),
+    path('string/', views.string),
+    path('post/', views.post),
+    path('header/', views.header),
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+
 
 # Create your views here.
 
 
 def index(request):
+    # user = request.GET.get('user')
+    user = request.GET.getlist('user')
+    return HttpResponse(f'{user}')
+
+
+def goods(request, cat_id, goods_id):
+    return HttpResponse(f"{cat_id}, {goods_id}")
+
+
+def string(request):
+    get = request.GET
+    print(get)
+    return HttpResponse(f'ok')
+
+
+def post(request):
+    print(request.POST)
+    return HttpResponse('ok')
+
+
+def header(request):
+    print(request.META)
+    print(request.method)
+    print(request.user)
     return HttpResponse('ok')
