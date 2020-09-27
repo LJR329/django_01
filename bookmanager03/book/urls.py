30338ee79224a81b92f5cf63384a4a1060de2b3b
李锦荣 <1813577274@qq.com>
2020-09-27 08:30:42 +0800

    自定义转换器

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -16,6 +16,16 @@ class MobileConverter:
         return str(value)
 
 
+# class Zi:
+#     regx = '1[3-9]\d{9}'
+# 
+#     def to_python(self, value):
+#         return int(value)
+#
+#     def to_usrl(self, value):
+#         return int(value)
+
+
 register_converter(MobileConverter, 'model')
 
 urlpatterns = [
